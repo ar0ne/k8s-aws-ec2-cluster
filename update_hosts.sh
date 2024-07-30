bb48f1a04a47b003b48cddec7e1204292fbe3124
ar0ne <camtbitakou@yandex.ru>
2024-07-30 23:39:18 +0400

    updated script for hosts generation

--- update_hosts.sh
+++ update_hosts.sh
@@ -4,6 +4,14 @@ SERVER_IP=$(aws cloudformation list-exports --query "Exports[?Name==\`server\`].
 NODE0_IP=$(aws cloudformation list-exports --query "Exports[?Name==\`node-0\`].Value" --no-paginate --output text)
 NODE1_IP=$(aws cloudformation list-exports --query "Exports[?Name==\`node-1\`].Value" --no-paginate --output text)
 
+if [[ -z $SERVER_IP ]]; then
+    echo "Server (control plane) node public IP not found. Abort"
+    exit
+fi
+
 echo $SERVER_IP server > hosts
 echo $NODE0_IP node-0 >> hosts
 echo $NODE1_IP node-1 >> hosts
+
+echo "updating /etc/hosts"
+cat hosts | sudo tee -a /etc/hosts
